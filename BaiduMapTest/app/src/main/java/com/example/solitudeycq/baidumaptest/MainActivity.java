5bf25755953182624b595c9cfc22a757d618199b
solitudeycq <solitudeycq@163.com>
2016-06-12 00:13:37 +0800

    代码完成，但是需处理WRITE_SETTINGS这一特殊权限，与SYSTEM_ALERT_WINDOW相似

--- BaiduMapTest/app/src/main/java/com/example/solitudeycq/baidumaptest/MainActivity.java
+++ BaiduMapTest/app/src/main/java/com/example/solitudeycq/baidumaptest/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.solitudeycq.baidumaptest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
 public class MainActivity extends AppCompatActivity {
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
 
+        mapView = (MapView)findViewById(R.id.map_view);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
     }
 }
